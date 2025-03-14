975a0b2807fcac1d0f2d24f2bddb97c619720e76
Jair Hernandez Dodino <jahernandez.ext@ayesa.com>
2025-03-14 18:52:30 +0100

    .gitignire

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -892,13 +892,33 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$settings['config_sync_directory'] = '../config/sync';
 
 include '../load.environment.php';
-switch (getenv('ENVIRONMENT')) {
-	case 'dev':
-		if (file_exists($app_root . '/' . $site_path . '/settings.dev.php')) {
-			include $app_root . '/' . $site_path . '/settings.dev.php';
-		}
-	break;
-}
+$driver = "mysql";
+$databases['default']['default'] = [
+  'database' => getenv('DATABASE_NAME'),
+  'username' => getenv('DATABASE_USER'),
+  'password' => getenv('DATABASE_PASSWORD'),
+  'prefix' => '',
+  'host' => getenv('DATABASE_HOST'),
+  'port' => getenv('DATABASE_PORT'),
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => $driver,
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+];
+
+$settings['migrate_source_connection'] = 'migrate';
+$settings['migrate_source_version'] = '7';
+$settings['migrate_file_public_path'] = '/var/www/html/web';
+$settings['migrate_file_private_path'] = '';
+$databases['migrate']['default'] = [
+  'database' => getenv('DB_MIGRATE_NAME'),
+  'username' => getenv('DB_MIGRATE_USER'),
+  'password' => getenv('DB_MIGRATE_PASSWORD'),
+  'host' => getenv('DB_MIGRATE_HOST'),
+  'driver' => $driver,
+  'port' => getenv('DB_MIGRATE_PORT'),
+  'prefix' => '',
+];
+$settings['config_sync_directory'] = '../config/sync';
