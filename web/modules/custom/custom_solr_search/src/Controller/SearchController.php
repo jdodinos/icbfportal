9ce5697f1aa636d8043144d78db898cdf0772ed3
Jair Hernandez <jdodinos@gmail.com>
2025-07-13 08:16:17 +0100

    modulos del buscador

--- web/modules/custom/custom_solr_search/src/Controller/SearchController.php
+++ web/modules/custom/custom_solr_search/src/Controller/SearchController.php
@@ -11,19 +11,39 @@ class SearchController extends ControllerBase {
   public function autocomplete(Request $request) {
     $string = $request->query->get('q');
     $matches = [];
-
+  
     if ($string) {
       $index = \Drupal::entityTypeManager()->getStorage('search_api_index')->load('icbf');
-      $query = $index->query()->keys($string)->range(0, 5);
-      $results = $query->execute();
-
-      foreach ($results as $item) {
-        $fields = $item->getFields();
-        $label = $fields['title']->getValues()[0] ?? 'Sin título';
-        $matches[] = ['value' => $label, 'label' => $label];
+      if ($index) {
+        $query = $index->query()
+		       ->keys($string)->range(0, 5);
+  
+        $results = $query->execute();
+  	
+        foreach ($results as $item) {
+	  $fields = $item->getFields();
+          	  
+	 \Drupal::logger('custom_solr_search')->notice('Autocomplete fields: @keys', [
+          '@keys' => implode(', ', array_keys($fields)),
+        ]);
+	  // Fallback a ss_title si es más simple
+          if (isset($fields['title'])) {
+            $label = $fields['title']->getValues()[0];
+	  }
+	  elseif(isset($fields['filename'])){
+	    $label = $fields['filename']->getValues()[0];
+	  }
+          else {
+            $label = 'Sin título';
+          }
+  
+	  $matches[] = ['value' => $label, 'label' => $label];
+        }
       }
-    }
-
+    }  
     return new JsonResponse($matches);
   }
+
 }
+
+
